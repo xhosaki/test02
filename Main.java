bb2fca967c062f5aa0faa125a7b0b968e4c82a17
kanazawa <e1q19024@oit.ac.jp>
2023-01-19 17:54:50 +0900

    プレイヤーの実装

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args){
         System.out.println("HERO!");
         Hero hero = new Hero();
+        Player player = new Player();
+        player.drawHeros();
+        player.showDeck();
         System.out.println(hero.toString());
 
     }
---
+++ Player.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+/*
+*プレイヤーはヒーローデッキを持つ
+* Deck:MonsterのArrayList
+*/
+public class Player {
+
+ArrayList<Hero> deck = new ArrayList<>();
+public void drawHeros() {
+
+for (int i = 0; i < 5; i++) {
+this.deck.add(new Hero());
+
+}
+
+}
+public void showDeck() {
+
+for (Hero h : this.deck){
+System.out.println(h);
+
+}
+
+}
+
+}
